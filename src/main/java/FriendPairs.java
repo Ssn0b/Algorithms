4f7c1fd20ed9cc3718b3200847255e67dd513a4c
levc4 <levc4kvlad@gmail.com>
2023-12-08 16:45:05 +0200

    Initial commit

---
+++ src/main/java/FriendPairs.java
@@ -0,0 +1,19 @@
+public class FriendPairs {
+    public static void main(String[] args) {
+        int n = 4;
+        System.out.println(countFriendsPairings(n));
+    }
+    static int countFriendsPairings(int n)
+    {
+        int[] dp = new int[n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            if (i <= 2)
+                dp[i] = i;
+            else
+                dp[i] = dp[i - 1] + (i - 1) * dp[i - 2];
+        }
+
+        return dp[n];
+    }
+}
